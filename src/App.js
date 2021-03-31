978a6e1659b6e15daae02682ae0f64e0c7752537
Axel Rasmussen <axl.rasmussen@gmail.com>
2021-03-30 21:11:27 -0300

    Basic algorithm idea

--- src/App.js
+++ src/App.js
@@ -3,24 +3,53 @@ import Reviews from './components/Reviews';
 import data from './reviews.json';
 import $ from 'jquery';
 
-
+let reviewsArr=[
+  <div id={"review-0"} className="review">
+      <img src={data.reviews[0].picture} alt={data.reviews[0].picture}></img>
+      <div className="review-text">
+          <p className="name">{data.reviews[0].name}</p>
+          <p className="role">{data.reviews[0].role.toUpperCase()}</p>
+          <p className="desc">{data.reviews[0].reviewDesc}</p>
+      </div>
+  </div>
+];
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      currentReview: 0
+      currentReview: 0,
+      reviews: reviewsArr
     }
 
     this.nextReview = this.nextReview.bind(this);
     this.previousReview = this.previousReview.bind(this);
   }
 
-
-
   nextReview() {
+
+    let nextReviewId = 0;
+
+    if(this.state.currentReview !== data.reviews.length -1){
+      nextReviewId = this.state.currentReview +1;
+    }
+
+    let nextReviewElem = (
+      <div id={"review-" + nextReviewId} className="review">
+          <img src={data.reviews[nextReviewId].picture} alt={data.reviews[nextReviewId].picture}></img>
+          <div className="review-text">
+              <p className="name">{data.reviews[nextReviewId].name}</p>
+              <p className="role">{data.reviews[nextReviewId].role.toUpperCase()}</p>
+              <p className="desc">{data.reviews[nextReviewId].reviewDesc}</p>
+          </div>
+      </div>
+    );
     
+    this.setState({
+      reviews: [reviewsArr[0], nextReviewElem]
+    })
+
     if(this.state.currentReview === data.reviews.length -1) {
     this.setState({
       currentReview: 0
@@ -30,6 +59,8 @@ class App extends React.Component {
       currentReview: this.state.currentReview + 1
     });
     }
+
+    $(".review").animate({right: "750px"}, 400);
   }
 
   previousReview() {    
@@ -57,7 +88,7 @@ class App extends React.Component {
           </div>
 
           <div className="review-parent">
-            <Reviews data={data.reviews} current={this.state.currentReview} prev={this.state.prevReview} next={this.state.nextReview} />
+            <Reviews data={this.state.reviews} current={this.state.currentReview} />
           </div>
 
           <div className="arrows">
--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -4,14 +4,7 @@ export default class Reviews extends Component {
     render() {
         return (
             <div className="review-set">
-                <div id={"review-" + this.props.current} className="review">
-                    <img src={this.props.data[this.props.current].picture} alt={this.props.data[this.props.current].picture}></img>
-                    <div className="review-text">
-                        <p className="name">{this.props.data[this.props.current].name}</p>
-                        <p className="role">{this.props.data[this.props.current].role.toUpperCase()}</p>
-                        <p className="desc">{this.props.data[this.props.current].reviewDesc}</p>
-                    </div>
-                </div>
+                {this.props.data.map(element => (element))}
             </div>
         )
     }
